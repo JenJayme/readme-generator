4e27d922d5988197b00afaf4da6142693b9c27d8
Jen Jayme <jeniferjayme@gmail.com>
2020-07-11 18:47:34 -0700

    Polished README template with working script. Minor adjustments needed on images.

--- NewREADME.md
+++ NewREADME.md
@@ -1,41 +1,71 @@
 
 
-## Project title 
-undefined
+<style>
+img[src*="#visuals"] {
+    width:500px;
+ }
 
-## Description
-undefined
+img[src*="#profile-pic] {
+    float: left;
+    display: block;
+    border-radius: 50%
+}
+ </style>
 
-## Demo
-undefined
+# Jen's Quick README-Maker
+![Travis Status](https://img.shields.io/travis/:jenjayme/:readme-generator)
+![License](https://img.shields.io/github/license/jenjayme/readme-generator)
+![Inquirer](https://img.shields.io/npm/v/inquirer)
 
-### Table of Contents
-undefined
+<hr>
 
-## Installation
-undefined
+Jen’s Quick README-Maker is a simple friendly tool that automates the task of generating a README file to accompany your apps.
 
-## License
-undefined
+<hr>
 
-## Contributing
-undefined
+## Contents | Quick Links
+[Getting Started](#start)  |  [Instructions](#instruct)  |  [Demo](#demo)  |  [Built With](#built)  |  [Author](#author)  |  [Visuals](#visuals)  [Contributing & Tests](Contributing.md)
 
-## Tests
+<hr>
+
+## [Getting Started](#start)
+From the command line, type index.js
+
+### You can view a demo here: 
+* ![Deployed URL](https://jenjayme.github.io/readme-generator#demo)
+
+### Download the app from Git Hub here:
+* [Repo URL](https://github.com/jenjayme/readme-generator/)
+
+__________
+
+## [Visuals](#visuals)
+
+![Screenshot](images/screenshot.png#visuals)
+
+![Example Gif](images/example.gif#visuals)
+__________
+
+## [Built With](#built)
+![Javascript]https://simpleicons.org/icons/javascript.svg
+![VS Code](https://simpleicons.org/icons/visualstudiocode.svg)
+![Markdown](https://simpleicons.org/icons/markdown.svg)
+
+__________
+
+## [Author](#author)
+Jen Jayme
+
+Web Developer
+
+Novato, CA
+
+[LinkedIn](https://www.linkedin.com/in/jenjayme)
+
+## Contributing and Tests
 undefined
 
 ## Questions
 undefined
 
-## Authored By
-Jen Jayme
-
-web dev in training
-
-Novato, CA
-
-LinkedIn: http://www.linkedin.com/jenjayme
-
-## Gif Example
-![Screenshot](undefined)
  
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-//UTILITIES NEEDED
+//UTILITIES & GLOBAL VARIABLES===============================================================
+
 const fs = require('fs');
 const util = require('util');
 const inquirer = require('inquirer');
-const axios = require('axios');
-const json2md = require("json2md")
 
 // const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -14,8 +13,34 @@ const userData = {};
 //ARRAY TO HOLD README FILE CONTENT
 const fileContent = [];
 
-//ARRAY OF OBJECTS FOR QUESTIONS, with input types and var names for key values. 
+//ARRAY OF OBJECTS FOR QUESTIONS, with input types and var names for key values.===============================================================
+
 let questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What's the title of your app or project? "
+    },
+    {
+        type: "input",
+        name: "introText",
+        message: "Enter a few sentences about the project for the intro section: "
+    },
+    {
+        type: "input",
+        name: "demo",
+        message: "Enter the link where a demo can be viewed (deployed link) - https:// "
+    },
+    {
+        type: "input",
+        name: "githubPage",
+        message: "Your Git Hub page - https://github.com/"
+    },
+    {
+        type: "input",
+        name: "githubRepo",
+        message: "Repo name on Git Hub - https://github.com/yourname/"
+    },
     {
         type: "input",
         name: "name",
@@ -29,78 +54,61 @@ let questions = [
     {
         type: "input",
         name: "bio",
-        message: "Please enter a tag line or brief bio statement:"
+        message: "Enter a short bio tag line (eg web developer):"
     },
     {
         type: "input",
         name: "linkedIn",
-        message: "Your linkedIn page? "
+        message: "Your linkedIn page - https://www.linkedin.com/in/"
     },
-    // {
-    //     type: "input",
-    //     name: "githubURL",
-    //     message: "Enter the link to the Repo on Git Hub: "
-    // },
-    // {
-    //     type: "input",
-    //     name: "title",
-    //     message: "What's the title of your app or project? "
-    // },
-    // {
-    //     type: "input",
-    //     name: "demo",
-    //     message: "Enter the link where a demo can be viewed (deployed link): "
-    // },
-    // {
-    //     type: "input",
-    //     name: "introText",
-    //     message: "Enter a few sentences to introduce the project and it's use: "
-    // },
-    // {
-    //     type: "input",
-    //     name: "instructions",
-    //     message: "Enter instructions to install and run the app: "
-    // },
-    // {
-    //     type: "input",
-    //     name: "screenshot",
-    //     message: "Enter the relative path for your screenshot: "
-    // },
-    // {
-    //     type: "input",
-    //     name: "example",
-    //     message: "Enter the relative path for your example gif: "
-    // },
-    // {
-    //     type: "input",
-    //     name: "shoutOuts",
-    //     message: "Any acknowledgements to include? "
-    // },
-    //add license, tests, questions
+    {
+        type: "input",
+        name: "contact",
+        message: "Best phone or email to contact you with questions?"
+    },
+    {
+        type: "input",
+        name: "instructions",
+        message: "Enter instructions to install and run the app: "
+    },
+    {
+        type: "input",
+        name: "images",
+        message: "have you saved a screenshot.png and an example.gif in the images folder? "
+    },
+    {
+        type: "input",
+        name: "shoutOuts",
+        message: "Any acknowledgements to include? "
+    },
+    //add tests, questions
 ];
 
-//PROMPTS USER FOR PERSONAL INFO
+//PROMPTS USER FOR PERSONAL INFO AND SAVES RESPONES TO USERDATA OBJECT===============================================================
+
 function getUserInformation() {
     return inquirer.prompt(questions).then(function (userInput) {
         console.log("userInput: ", userInput);
+        userData.title = userInput.title,
+        userData.introText = userInput.introText,
+        userData.instructions = userInput.instructions,
+        userData.demo = userInput.demo,
+        userData.githubPage = userInput.githubPage,
+        userData.githubRepo = userInput.githubRepo,
         userData.name = userInput.name,
         userData.location = userInput.location,
         userData.bio = userInput.bio,
         userData.linkedIn = userInput.linkedIn,
-            // userData.githubURL = userInput.githubURL,
-            // userData.title = userInput.title,
-            // userData.demo = userInput.demo,
-            // userData.introText = userInput.introText,
-            // userData.instructions = userInput.instructions,
-            // userData.screenshot = userInput.screenshot,
-            // userData.example = userInput.example,
-            // userData.shoutOuts = userInput.shoutOuts
-            // add installation, license, tests, questions
+        userData.contact = userInput.contact,
+        userData.images = userInput.images,
+        userData.shoutOuts = userInput.shoutOuts
+        // dd installation, license, tests, questions
         console.log("userData: ", userData);
     })
 };
 
-// writeFile function with filename, content and callback function
+// CREATE NEW README FILE WITH USERDATA. GRABS DATA FROM USERDATA OBJ, INSERTS DATA INTO STRING WITH MD HEADINGS, STRINGIFIES THAT, AND WRITES IT ALL TO NEW README.===============================================================
+
 function writeFile(userData) {
     var userDataStr = generateMarkdown(userData);
 
@@ -110,49 +118,85 @@ function writeFile(userData) {
     });
 };
 
+//README TEMPLATE===============================================================
+
 function generateMarkdown(userData) {
     return `
 
-## Project title 
-${userData.title}
+<style>
+img[src*="#visuals"] {
+    width:500px;
+ }
 
-## Description
+img[src*="#profile-pic] {
+    float: left;
+    display: block;
+    border-radius: 50%
+}
+ </style>
+
+# ${userData.title}
+![Travis Status](https://img.shields.io/travis/:jenjayme/:readme-generator)
+![License](https://img.shields.io/github/license/jenjayme/readme-generator)
+![Inquirer](https://img.shields.io/npm/v/inquirer)
+<hr>
+
 ${userData.introText}
 
-## Demo
-${userData.githubURL}
+<hr>
 
-### Table of Contents
-${userData.toc}
+## Contents | Quick Links
+[Getting Started](#start)     |     [Instructions](#instruct)     |     [Demo](#demo)     |     [Built With](#built)     |     [Author](#author)     |     [Visuals](#visuals)     |     [Contributing & Tests](Contributing.md)
 
-## Installation
+<hr>
+
+## [Getting Started](#start)
+
+### Download the app from Git Hub here:
+* [Repo URL](https://github.com/${userData.githubPage}/${userData.githubRepo}/)
+
+### To run: 
 ${userData.instructions}
 
-## License
-${userData.license}
+### You can view a demo here: 
+* ![Deployed URL](https://${userData.demo}#demo)
 
-## Contributing
-${userData.contributing}
+<hr>
 
-## Tests
-${userData.test}
+## [Visuals](#visuals)
 
-## Questions
-${userData.questions}
+![Screenshot](images/screenshot.png#visuals)
 
-## Authored By
+![Example Gif](images/example.gif#visuals)
+
+<hr>
+
+## [Built With](#built)
+![Javascript]https://simpleicons.org/icons/javascript.svg
+![VS Code](https://simpleicons.org/icons/visualstudiocode.svg)
+![Markdown](https://simpleicons.org/icons/markdown.svg)
+
+<hr>
+
+## [Author](#author)
 ${userData.name}\n
 ${userData.bio}\n
 ${userData.location}\n
-LinkedIn: ${userData.linkedIn}
+[LinkedIn](https://www.linkedin.com/in/${userData.linkedIn})
 
-## Gif Example
-![Screenshot](${userData.screenshot})
+## Questions
+For questions or to discuss collaborations on this or other projects, contact me at:
+${userData.contact}
+
  `;
 }
 
+//END README TEMPLATE===============================================================
+
+//MAKE MARKDOWN DATA AVAILABLE OUTSIDE BROWSER 
 module.exports = generateMarkdown;
 
+//CALL FUNCTIONS===============================================================
 
 async function init() {
     await getUserInformation();
